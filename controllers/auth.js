d45694d92e7e7ed717b5d52c1e2dffe462a3f4b6
VolodymyrVolynets <81035911+VolodymyrVolynets@users.noreply.github.com>
2023-03-18 15:20:48 +0000

    registration fixe; input validation added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
-const mysql = require('mysql')
+const mysql = require('mysql2/promise')
+const bcrypt = require('bcrypt')
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
     host: process.env.DBHOST,
     port: process.env.DBPORT,
     user: process.env.DBUSER,
@@ -8,38 +9,60 @@ const connection = mysql.createConnection({
     database: process.env.DBNAME
 })
 
-// TODO - Manage users input
-// TODO - check for existing usernames
-
-exports.register = (req, res) => {
+exports.register = async (req, res) => {
     const { username, password, confirmPassword } = req.body
 
-    if (password == "") {
-        res.render('pages/registration',{
-            message: 'Incorrect Password',
-            user: {
-                username: username
-            }
+    //Check for valid input
+    if (password != confirmPassword) {
+        return res.render('pages/registration', {
+            message: 'Passwords do not match', 
+            user: { username: username } 
         })
-    } else if (password != confirmPassword) {
-        res.render('pages/registration',{
-            message: 'Passwords do not match',
-            user: {
-                username: username
-            }
+    } else if (!isValidInput(password) || !isValidInput(username)) {
+        return res.render('pages/registration', {
+            message: 'Password and username should contains digits or lettes and 5-16 symbols length', 
+            user: { username: username } 
         })
     }
 
-    connection.query("INSERT INTO users (username, password) VALUES (?, ?)", [username, password], function (err, result) {
-        if (err) {
-            res.render('pages/registration',{
-                message: 'Error in DataBase',
-                user: {
-                    username: username
-                }
+    //if inputs valid check if user already exist
+    try {
+        const allUsersWithUsername = await executeMYSQL("SELECT * from users WHERE username = ?", [username])
+        if (allUsersWithUsername.length >= 1) {
+            return res.render('pages/registration', {
+                message: 'Username already taken', 
+                user: { username: username } 
             })
         } else {
-            res.render('pages/registration')
+            //if user not exist create new user and redirect to a home page
+            const hashedPass = await hash(password)
+            executeMYSQL("INSERT INTO users (username, password) VALUES (?, ?)", [username, hashedPass])
+            return res.redirect('/')
         }
-    })
+    } catch (err) {
+        //if error with db
+        console.log(err)
+        res.render('pages/registration'), {
+            message: 'Unknown Error',
+            user: {
+                username: username
+            }
+        }
+    }
+}
+
+function isValidInput(input) {
+    const regex = new RegExp("^[a-zA-Z0-9]{5,16}$");
+    const isValid = regex.test(input);
+    return isValid;
+}
+
+async function executeMYSQL(query, params) {
+    const [result] = await connection.execute(query, params)
+    return result
+}
+
+async function hash(toHash) {
+    const hashed = await bcrypt.hash(toHash, 10)
+    return hashed
 }
